3cda843e3581c57728f2f8d59b42ea0d94d53cd8
Lincoln Teixeira <lincoln@vexter.com.br>
2020-11-30 14:40:51 -0300

    create project and make initial configuration

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
+const Routes = () => (
+    <BrowserRouter> 
+        <Switch>
+            <h1>Hello World</h1>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
