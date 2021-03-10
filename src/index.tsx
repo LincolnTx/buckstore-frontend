4dad0cd97f3bd7fb3f4fc73fa31580580868d6b4
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-03-10 14:42:01 -0300

    chore: remove real info fron .env
    Create a env for dev and one for production
    Config dotenv to use corret env base on prd ou dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import dotenv from 'dotenv';
+dotenv.config({ path: `../env.${process.env.NODE_ENV}` });
 
 ReactDOM.render(
   <React.StrictMode>
