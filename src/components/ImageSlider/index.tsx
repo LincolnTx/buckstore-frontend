13e4745b199b0073763d4579626462205811115a
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-09-30 12:41:40 -0300

    refactor: fix style to list products page

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,10 +1,21 @@
 import './styles.css';
 
-function ImageSlider() {
+interface Props {
+  images: string[];
+  height: number;
+  width: number;
+}
+
+function ImageSlider({images, height, width, ...rest}: Props) {
   
   
   return (
-    <h1>Image slider no estilo do da kabum</h1>
+    <img 
+      src={images[0]} 
+      alt="imagens do produto" 
+      height={`${height}%`}
+      width={`${width}%`}
+    />
   );
 }
     
