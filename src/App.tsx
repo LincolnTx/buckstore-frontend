4a50c5ccb4d2d8354e719af13a0d8aabde8f4cf1
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-10-03 17:43:04 -0300

    feat: create a context for card shopping itens

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './global.css';
 
 import Routes from './routes';
 import {AuthProvider} from './contexts/auth';
+import { ShoppingCartProvider } from './contexts/shoppingCart';
 
 function App() {
   return (
     <AuthProvider>
+      <ShoppingCartProvider>
       <Routes />
+      </ShoppingCartProvider>
     </ AuthProvider>
   );
 }
---
+++ src/contexts/shoppingCart.tsx
@@ -0,0 +1,61 @@
+import { createContext, useState } from 'react';
+
+export interface ShoppingCartContextType {
+    addItem(item: ShoppingItem): Promise<void>;
+    getItens(): ShoppingItem[];
+    removeItem(id: string): void;
+    editItem(newItem: ShoppingItem): void;
+    cleanCart(): void;
+
+}; 
+
+export interface ShoppingItem {
+    productId: string
+    productName: string;
+    price: number;
+    quantity: number;
+}
+
+const ShoppingCartContext = createContext<ShoppingCartContextType>({} as ShoppingCartContextType);
+
+export const ShoppingCartProvider: React.FC = ({children}) => {
+    const [shoppingCart, setShoppingCart] = useState<ShoppingItem[]>([{productId: "item de merda", productName: "merda", price: 18, quantity: 1}]);
+
+    function addItem(item: ShoppingItem): Promise<void> {
+        console.log('adding an item')
+        setShoppingCart(current => [...current, item])
+
+        return new Promise((resolve) => {
+            resolve();
+         });
+    }
+
+    function getItens(): ShoppingItem[] {
+        return shoppingCart;
+    }
+
+    function removeItem(id: string): void {
+        setShoppingCart(shoppingCart.filter(item => item.productId !== id));
+    }
+
+    function editItem(newItem: ShoppingItem): void {
+        const index = shoppingCart.findIndex(item => item.productId === newItem.productId);
+        const currentCart = shoppingCart;
+        currentCart[index] = newItem;
+
+        setShoppingCart(currentCart);
+    }
+
+    function cleanCart(): void {
+
+    }
+
+  return (
+    <ShoppingCartContext.Provider 
+        value= {{ addItem, getItens, removeItem, editItem, cleanCart }}>
+        {children}
+    </ShoppingCartContext.Provider>
+  );
+};
+
+export default ShoppingCartContext;
