5fc2614e89e96782f17fe8619ad761ec3deaf44f
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-10-25 21:01:31 -0300

    refactor: finish  adding parts for comfirm part of form
    Styling confirm form
    change little things on other styles components

--- src/components/Confirm/index.tsx
+++ src/components/Confirm/index.tsx
@@ -3,10 +3,13 @@ import { FormEvent, useState, useEffect } from 'react';
 
 import { OrderCheckoutState } from '../../pages/OrderCheckout';
 import { Api } from '../../helpers/api';
+import Collapsible from '../Collapsible';
 
 import './styles.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';  
+import defaultImage from '../../helpers/DefaultImage';
+import { ShoppingItem } from '../../contexts/shoppingCart';
 
 interface Props {
     nextStep() : void;
@@ -121,17 +124,172 @@ export function Confirm({nextStep, prevStep, values, isNewCard}: Props) {
                 "Erro ao realizar seu pedido, tente novamente mais tarde, ou entre em contato."
             );
         }
+    }
 
+    function verifyErrorClass(field: string) {
+        if (field === undefined || field === null || field === '') {
+            return 'invalid-input'
+        }
 
     }
+
+    function handleProductTotalPrice(quantiy: number, price: number) {
+        return (quantiy * price).toLocaleString("pt-br");
+    }
+
+    function handleProductCover(product: ShoppingItem) {
+        if (product.image) {
+            return product.image;
+        }
+    
+        return defaultImage;
+      }
+
     return (
        <div className="confirm-main-container">
 
-           <button onClick={() => handleOrdering()}
-                className={`button ${isValid ? '' : 'closed'}`}
-           >
-               Relizar pedido
-            </button>
+            <form>
+                <div className="input-group">
+                    <input 
+                        type="text" 
+                        placeholder='Cep'
+                        defaultValue={values.zipcode}
+                        name={values.zipcode}
+                        disabled
+                        className={verifyErrorClass(values.zipcode)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder='Estado'
+                        defaultValue={values.state}
+                        name={values.state}
+                        disabled
+                        className={verifyErrorClass(values.state)}
+                    />
+                </div>
+
+                <div className="input-group">
+                    <input 
+                        type="text" 
+                        placeholder='Bairro'
+                        defaultValue={values.district}
+                        name={values.district}
+                        disabled
+                        className={verifyErrorClass(values.district)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder='Cidade'
+                        defaultValue={values.city}
+                        name={values.city}
+                        disabled
+                        className={verifyErrorClass(values.city)}
+                    />
+                </div>
+
+                <div className="input-group">
+                    <input 
+                        type="text" 
+                        placeholder='Número do cartão'
+                        defaultValue={values.cardNumber}
+                        name={values.cardNumber}
+                        disabled
+                        className="input-cover"
+                    />
+                    <input 
+                        type="text" 
+                        placeholder='CVV'
+                        defaultValue={values.cardSecurityNumber}
+                        name={values.cardSecurityNumber}
+                        disabled
+                        className="input-cover"
+                    />
+                   
+                </div>
+
+                <div className="input-group">
+                    <input 
+                        type="text" 
+                        placeholder='Apelido do cartão'
+                        defaultValue={values.cardAlias}
+                        name={values.cardAlias}
+                        disabled
+                        className={verifyErrorClass(values.cardAlias)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder='Nome impresso no cartão'
+                        defaultValue={values.cardHolderName}
+                        name={values.cardHolderName}
+                        disabled
+                        className={verifyErrorClass(values.cardHolderName)}
+                    />
+                   
+                </div>
+
+                <div className="input-group">
+                    <input 
+                        type="text" 
+                        placeholder='Data de expiração do cartao'
+                        defaultValue={new Date(values.cardExpiration).toLocaleDateString()}
+                        name={values.cardExpiration}
+                        disabled
+                        className={verifyErrorClass(values.cardExpiration)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder='cpf'
+                        defaultValue={values.cpf}
+                        name={values.cpf}
+                        disabled
+                        className={verifyErrorClass(values.cpf)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder='Número de entrega'
+                        defaultValue={values.addressNumber}
+                        name={values.addressNumber}
+                        disabled
+                        className={verifyErrorClass(values.addressNumber)}
+                    />
+                </div>
+            </form>
+            <Collapsible 
+                title="Itens do pedido"
+            >
+                <ul className={`${values.orderItems.length <= 0 ? 'invisible' : ''}`}>
+                    {values.orderItems.map(item => (
+                        <li key={item.productId}>
+                            <span>{item.productName}</span>
+                            <div className="info-container">
+                                <img src={handleProductCover(item)} alt="" />
+                                <div>
+                                    <span>Quantidade: {item.quantity}</span>
+                                    <span>R$ {handleProductTotalPrice(item.price, item.quantity)}</span>
+                                </div>
+                            </div>
+                                
+                        </li>
+                        
+                    ))}
+                </ul>
+
+                <div 
+                    className={`nothing-to-see ${values.orderItems.length > 0 ? 'invisible' : ''}`}
+                >
+                    <span> Não há nada aqui</span>
+                </div>
+
+                
+            </Collapsible>
+            <div className="buttons-container">
+                <button className="button" onClick={() => prevStep()}>Voltar</button>
+                <button onClick={() => handleOrdering()}
+                    className={`button ${isValid ? '' : 'closed'}`}
+            >
+                Relizar pedido
+                </button>
+            </div>
        </div>
     );
 }
